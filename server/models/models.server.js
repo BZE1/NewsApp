ee49a9784c92a7746f110fd0acf74876f7152ded
TSN Student <student@ulem.org>
2018-08-14 13:19:08 -0400

    test mlab

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -4,8 +4,12 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds129946.mlab.com:29946/heroku_z7t7l4w7'; // use yours
+  connectionString += '@ds119692.mlab.com:19692/heroku_fm6kwhbp'; // use yours
 }
+
+// vwv__________connection string____________________________
+// mongodb://<dbuser>:<dbpassword>@ds119692.mlab.com:19692/heroku_fm6kwhbp
+
 
 var mongoose = require("mongoose");
 
